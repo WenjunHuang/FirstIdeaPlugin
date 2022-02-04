4bb1c04bd2236fc5632af01f82eabf0dadc7ea92
WenjunHuang <wenjun.huang80@gmail.com>
2022-02-04 18:44:40 +0800

    update

--- build.sbt
+++ build.sbt
@@ -1,18 +1,24 @@
 import org.jetbrains.sbtidea.Keys._
 
-lazy val myAwesomeFramework =
-  project.in(file("."))
+lazy val firstIdeaPlugin =
+  project
+    .in(file("."))
     .enablePlugins(SbtIdeaPlugin)
     .settings(
       version := "0.0.1-SNAPSHOT",
-      scalaVersion := "2.13.2",
-      ThisBuild / intellijPluginName := "My Awesome Framework",
-      ThisBuild / intellijBuild      := "203.7148.15",
-      ThisBuild / intellijPlatform   := IntelliJPlatform.IdeaCommunity,
-      Global    / intellijAttachSources := true,
+      scalaVersion := "3.1.1",
+      ThisBuild / intellijPluginName := "FooBrowser",
+      ThisBuild / intellijBuild := "213.6777.52",
+      ThisBuild / intellijPlatform := IntelliJPlatform.IdeaCommunity,
+      Global / intellijAttachSources := true,
       Compile / javacOptions ++= "--release" :: "11" :: Nil,
       intellijPlugins += "com.intellij.properties".toPlugin,
-      libraryDependencies += "com.eclipsesource.minimal-json" % "minimal-json" % "0.9.5" withSources(),
-      unmanagedResourceDirectories in Compile += baseDirectory.value / "resources",
-      unmanagedResourceDirectories in Test    += baseDirectory.value / "testResources",
+      libraryDependencies ++=
+        Seq(
+          "com.eclipsesource.minimal-json" % "minimal-json" % "0.9.5" withSources (),
+          "org.scala-lang.modules" %% "scala-swing" % "3.0.0",
+          "org.typelevel" %% "cats-effect" % "3.3.5"
+        ),
+      Compile / unmanagedResourceDirectories += baseDirectory.value / "resources",
+      Test / unmanagedResourceDirectories += baseDirectory.value / "testResources"
     )
--- src/main/scala/org/jetbrains/scala/samples/ui/MyToolWindowFactory.scala
+++ src/main/scala/org/jetbrains/scala/samples/ui/MyToolWindowFactory.scala
@@ -5,11 +5,65 @@ package org.jetbrains.scala.samples.ui
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.wm.{ToolWindow, ToolWindowFactory}
 import com.intellij.ui.components.JBLabel
-import org.jetbrains.scala.samples.SamplePluginBundle
+import com.intellij.ui.content.{Content, ContentFactory}
+import com.intellij.uiDesigner.core.{GridConstraints, GridLayoutManager}
+
+import java.util.Calendar
+import javax.swing.{ImageIcon, JButton, JLabel, JPanel}
+import scala.swing.*
 
 class MyToolWindowFactory extends ToolWindowFactory {
 
-  override def createToolWindowContent(project: Project, toolWindow: ToolWindow): Unit =
-    toolWindow.getComponent.add(new JBLabel(SamplePluginBundle.message("my.cool.tool.window")))
+  override def createToolWindowContent(
+      project: Project,
+      toolWindow: ToolWindow
+  ): Unit = {
+    val contentFactory = ContentFactory.SERVICE.getInstance
+    val content = contentFactory.createContent(
+      MyToolWindowFactory.createContent(),
+      "",
+      false
+    )
+    toolWindow.getContentManager.addContent(content)
+  }
+}
 
+object MyToolWindowFactory {
+  def createContent(): JPanel = {
+    val content = new JPanel()
+    val layout = new GridLayoutManager(2, 5)
+    content.setLayout(layout)
+
+    val currentDate = new JLabel()
+    currentDate.setText("Date2")
+    val constraints = new GridConstraints()
+    constraints.setRow(0)
+    constraints.setColumn(1)
+    constraints.setRowSpan(1)
+    constraints.setColSpan(1)
+    content.add(currentDate, constraints)
+
+    val refreshToolWindowButton = new JButton("Refresh")
+    refreshToolWindowButton.addActionListener(e => {
+      val ci = Calendar.getInstance()
+      currentDate.setText(s"${ci.get(Calendar.DAY_OF_MONTH)}/${ci
+        .get(Calendar.MONTH) + 1}/${ci.get(Calendar.YEAR)}")
+      currentDate.setIcon(
+        new ImageIcon(
+          classOf[MyToolWindowFactory].getResource(
+            "/toolWindow/Calendar-icon.png"
+          )
+        )
+      )
+    })
+    constraints.setRow(1)
+    constraints.setColumn(1)
+    content.add(refreshToolWindowButton, constraints)
+
+    content
+  }
+
+//  def createContent2() = new GridBagPanel{
+//
+//  }
 }
